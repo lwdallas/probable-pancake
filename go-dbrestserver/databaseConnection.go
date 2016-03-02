bd069264105b8ef7f4742b8b0dd6332fd3dade12
lonniewebb <lonnie@lonniewebb.com>
2016-03-01 20:30:22 -0600

    moving files around

---
+++ go-dbrestserver/databaseConnection.go
@@ -0,0 +1,12 @@
+package main
+
+import "time"
+
+type DatabaseConnection struct {
+	Id        int       `json:"id"`
+	Name      string    `json:"name"`
+	Completed bool      `json:"completed"`
+	Due       time.Time `json:"due"`
+}
+
+type DatabaseConnections []DatabaseConnection
